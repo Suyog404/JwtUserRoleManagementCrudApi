eb4ddd776d3ad5e205b17c5865399f761079f587
Suyog404 <suyogdhakal1@gmail.com>
2024-10-09 11:14:30 +0545

    First Commit

---
+++ API/Dtos/LoginDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Email { get; set;} = string.Empty;
+
+        [Required]
+        public string Password {get;set;} = string.Empty;
+        
+    }
+}
